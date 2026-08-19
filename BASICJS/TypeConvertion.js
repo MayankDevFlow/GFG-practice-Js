8e3c9fd7e9901cc733e348acfd004a740fe90012
mayank gohiya <dtmayankgohiya@gmail.com>
2026-08-19 21:37:32 +0530

    Remove commented script reference and add TypeConvertion.js file

---
+++ BASICJS/TypeConvertion.js
@@ -0,0 +1,30 @@
+// Thay  All are false values 
+
+let a = false
+let b = 0
+let c = -0
+let d = 0n
+let e = ""
+let f = null
+let g = undefined
+let h = NaN
+
+// Excluding Above false value  all are Truthy Values 
+
+console.log(Boolean(a));
+console.log(Boolean(b));
+console.log(Boolean(c));
+console.log(Boolean(d));
+console.log(Boolean(e));
+console.log(Boolean(f));
+console.log(Boolean(g));
+console.log(Boolean(h));
+
+let p = 0n
+let i = 0 
+let j = -0
+let k = false
+let l =""
+let m = null 
+let n = undefined
+let o = NaN
