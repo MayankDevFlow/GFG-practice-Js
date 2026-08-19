0b83884e1598fdd0c9d9e12a6cc4ce2e4c860f81
mayank gohiya <dtmayankgohiya@gmail.com>
2026-08-19 21:47:08 +0530

    Refactor TypeConvertion.js: Update truthy and falsy value examples and adjust variable assignments

--- BASICJS/TypeConvertion.js
+++ BASICJS/TypeConvertion.js
@@ -11,20 +11,29 @@ let h = NaN
 
 // Excluding Above false value  all are Truthy Values 
 
-console.log(Boolean(a));
-console.log(Boolean(b));
-console.log(Boolean(c));
-console.log(Boolean(d));
-console.log(Boolean(e));
-console.log(Boolean(f));
-console.log(Boolean(g));
-console.log(Boolean(h));
+// console.log(Boolean(a));
+// console.log(Boolean(b));
+// console.log(Boolean(c));
+// console.log(Boolean(d));
+// console.log(Boolean(e));
+// console.log(Boolean(f));
+// console.log(Boolean(g));
+// console.log(Boolean(h));
 
-let p = 0n
-let i = 0 
-let j = -0
-let k = false
-let l =""
-let m = null 
-let n = undefined
-let o = NaN
+let p = -1 
+let i = 1 
+let j = "0"
+let k = "false"
+let l = []
+let m = {}
+let o = Infinity
+let n = -Infinity
+
+console.log(Boolean(o));
+console.log(Boolean(n));
+console.log(Boolean(m));
+console.log(Boolean(l));
+console.log(Boolean(k));
+console.log(Boolean(j));
+console.log(Boolean(i));
+console.log(Boolean(p));
